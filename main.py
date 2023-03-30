4a8b665e6d61538878cdf610c7627bba75cc9e3e
Sor1na <104823704+Sor1na@users.noreply.github.com>
2023-03-30 06:57:33 +0300

    Add files via upload
    Бета-Кнопки

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ init(autoreset=True)  # for colorama
 
 bot = telebot.TeleBot("6290776101:AAEsU7xSYtxu2zWeGqjfh3wJ06tr9RPaiYA")
 
-
+chat_id = 12345
 # start command and func
 @bot.message_handler(commands=['start'])  # start command
 def start(message):  # start func
@@ -20,12 +20,18 @@ def start(message):  # start func
           str(message.from_user.first_name), "Фамилия:", Fore.LIGHTGREEN_EX +
           str(message.from_user.last_name), "Ник:", Fore.LIGHTGREEN_EX +
           str(message.from_user.username), '////', Fore.LIGHTRED_EX + str(current_time))
+    #bot.reply_to(message,
+     #             f'Привет, {message.from_user.first_name} ! Начнем игру?.'
+      #           )
 
-    bot.reply_to(message,
-                 f'Привет, {message.from_user.first_name} ! Начнем игру?.'
-                 )
+    
+    button_1 = types.InlineKeyboardButton('Решение 1(да)', callback_data='решение 1')
+    button_2 = types.InlineKeyboardButton('Решение 2(нет)', callback_data='решение 2')
 
-
-
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(button_1)
+    keyboard.add(button_2)
+        
+    bot.send_message(message.chat.id, text =  f'Привет, {message.from_user.first_name} ! Начнем игру?.', reply_markup=keyboard)
 
 bot.polling(none_stop=True)
