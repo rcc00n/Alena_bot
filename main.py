092386ff08be65fd07e138a4b78afa4e2e210e8c
rcc00n <123768783+rcc00n@users.noreply.github.com>
2023-03-30 00:01:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import telebot
 from telebot import types
-from time import *
 from colorama import init, Fore
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+import csv
+import time
+import schedule
 
 init(autoreset=True)  # for colorama
 
 bot = telebot.TeleBot("6290776101:AAEsU7xSYtxu2zWeGqjfh3wJ06tr9RPaiYA")
 
-chat_id = 12345
-# start command and func
-@bot.message_handler(commands=['start'])  # start command
-def start(message):  # start func
-    current_time = strftime("%a %b %d %H:%M:%S %Y", localtime())
-    print(Fore.LIGHTBLUE_EX + "/start", "спросил:", "ID", Fore.LIGHTGREEN_EX + str(message.from_user.id), "Имя:",
-          Fore.LIGHTGREEN_EX +
-          str(message.from_user.first_name), "Фамилия:", Fore.LIGHTGREEN_EX +
-          str(message.from_user.last_name), "Ник:", Fore.LIGHTGREEN_EX +
-          str(message.from_user.username), '////', Fore.LIGHTRED_EX + str(current_time))
-    #bot.reply_to(message,
-     #             f'Привет, {message.from_user.first_name} ! Начнем игру?.'
-      #           )
+def save_to_csv(user_id, username, decision):
+    with open('data.csv', mode='a', newline='') as data_file:
+        data_writer = csv.writer(data_file)
+        data_writer.writerow([user_id, username, decision])
 
-    
+@bot.message_handler(commands=['start'])
+def start(message):
     button_1 = types.InlineKeyboardButton('Решение 1(да)', callback_data='решение 1')
-    button_2 = types.InlineKeyboardButton('Решение 2(нет)', callback_data='решение 2')
-
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(button_1)
-    keyboard.add(button_2)
-        
-    bot.send_message(message.chat.id, text =  f'Привет, {message.from_user.first_name} ! Начнем игру?.', reply_markup=keyboard)
+
+    bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name} ! Начнем игру?.', reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    user_id = call.from_user.id
+    username = call.from_user.username
+
+    if call.data == 'решение 1':
+        save_to_csv(user_id, username, 'решение 1')
+        button_2 = types.InlineKeyboardButton('Решение 2(да)', callback_data='решение 2')
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(button_2)
+
+        bot.send_message(call.message.chat.id, text=f'Вы выбрали "Решение 1(да)". Хороший выбор! А теперь выберите еще одну опцию:', reply_markup=keyboard)
+
+    elif call.data == 'решение 2':
+        save_to_csv(user_id, username, 'решение 2')
+        button_3 = types.InlineKeyboardButton('Решение 3(да)', callback_data='решение 3')
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(button_3)
+
+        bot.send_message(call.message.chat.id, text=f'Отлично, теперь выберите еще одну опцию:', reply_markup=keyboard)
+
+    elif call.data == 'решение 3':
+        save_to_csv(user_id, username, 'решение 3')
+        bot.send_message(call.message.chat.id, text='Вы выбрали "Решение 3(да)". Поздравляем, вы выиграли!', reply_markup=types.ReplyKeyboardRemove())
+
 
 bot.polling(none_stop=True)
