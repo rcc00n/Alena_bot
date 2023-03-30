ccbe2f9f7d0b20282737e3406ce78c360fdb7355
rcc00n <123768783+rcc00n@users.noreply.github.com>
2023-03-30 10:19:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import telebot
 from telebot import types
-from colorama import init, Fore
 import csv
+import datetime
 import time
-import schedule
-
-init(autoreset=True)  # for colorama
 
 bot = telebot.TeleBot("6290776101:AAEsU7xSYtxu2zWeGqjfh3wJ06tr9RPaiYA")
+
+users_data = {}
 
 def save_to_csv(user_id, username, decision):
     with open('data.csv', mode='a', newline='') as data_file:
         data_writer = csv.writer(data_file)
         data_writer.writerow([user_id, username, decision])
+
+def update_user_data(user_id, stage, time):
+    if user_id not in users_data:
+        users_data[user_id] = {'stage': stage, 'time': time}
+    else:
+        users_data[user_id]['stage'] = stage
+        users_data[user_id]['time'] = time
+
+def check_and_send_reminders():
+    now = datetime.datetime.now()
+    for user_id, data in users_data.items():
+        elapsed_time = now - data['time']
+        if elapsed_time > datetime.timedelta(days=1) and data['stage'] != 'решение 3':
+            bot.send_message(user_id, text='Привет! Не забудь завершить игру! Попробуй еще раз.')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -20,15 +33,20 @@ def start(message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(button_1)
 
+    update_user_data(message.chat.id, 'start', datetime.datetime.now())
+
     bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name} ! Начнем игру?.', reply_markup=keyboard)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
     user_id = call.from_user.id
     username = call.from_user.username
+    now = datetime.datetime.now()
 
     if call.data == 'решение 1':
         save_to_csv(user_id, username, 'решение 1')
+        update_user_data(user_id, 'решение 1', now)
+
         button_2 = types.InlineKeyboardButton('Решение 2(да)', callback_data='решение 2')
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(button_2)
@@ -37,6 +55,8 @@ def handle_callback(call):
 
     elif call.data == 'решение 2':
         save_to_csv(user_id, username, 'решение 2')
+        update_user_data(user_id, 'решение 2', now)
+
         button_3 = types.InlineKeyboardButton('Решение 3(да)', callback_data='решение 3')
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(button_3)
@@ -45,7 +65,19 @@ def handle_callback(call):
 
     elif call.data == 'решение 3':
         save_to_csv(user_id, username, 'решение 3')
+        update_user_data(user_id, 'решение 3', now)
+
         bot.send_message(call.message.chat.id, text='Вы выбрали "Решение 3(да)". Поздравляем, вы выиграли!', reply_markup=types.ReplyKeyboardRemove())
 
+def polling_with_reminders():
+    while True:
+        try:
+            bot.polling(none_stop=True, interval=0, timeout=20)
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+        finally:
+            check_and_send_reminders()
 
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    polling_with_reminders()
